c16a1edda60f8895d4571633d8b9886404dae58c
luke <test@gmail.com>
2025-03-25 07:49:41 -0300

    update

---
+++ tools/convert-ct2.cmd
@@ -0,0 +1,1 @@
+ct2-transformers-converter --model models/whisper-large-v2-finetune --output_dir models/whisper-large-v2-finetune-ct2 --copy_files tokenizer.json preprocessor_config.json --quantization float16
